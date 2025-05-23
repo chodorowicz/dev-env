87884d6ab33d88003f6145182963f1b9aef88fa7
Jakub Chodorowicz <jakub@chodorowicz.com>
2025-05-23 19:53:53 +0200

    load brew env in setup base
    Signed-off-by: Jakub Chodorowicz <jakub@chodorowicz.com>

--- setup-scripts/base.sh
+++ setup-scripts/base.sh
@@ -5,5 +5,7 @@ if ! command -v brew &>/dev/null; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 brew install dotter
 brew install --cask 1password
