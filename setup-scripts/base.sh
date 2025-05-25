16dfea7516e3c33e651621c05f769a352c555346
Jakub Chodorowicz <jakub@chodorowicz.com>
2025-05-25 17:34:51 +0200

    Properly check if homebrew is installed
    Signed-off-by: Jakub Chodorowicz <jakub@chodorowicz.com>

--- setup-scripts/base.sh
+++ setup-scripts/base.sh
@@ -2,10 +2,18 @@
 
 # install homebrew if it is not already installed
 if [ -d "/opt/homebrew/bin" ]; then
+  echo "Homebrew is already installed"
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+else
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-eval "$(/opt/homebrew/bin/brew shellenv)"
+# check if dotter is installed
+if ! command -v dotter &>/dev/null; then
+  brew install dotter
+fi
 
-brew install dotter
-brew install --cask 1password
+# check if 1password is installed
+if ! command -v 1password &>/dev/null; then
+  brew install --cask 1password
+fi
