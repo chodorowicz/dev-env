872246d6a0cb401ec6e1163af53c70de51d24330
Jakub Chodorowicz <jakub@chodorowicz.com>
2025-05-25 17:48:30 +0200

    macOS settings: disable autocorrection, tweak finder
    Signed-off-by: Jakub Chodorowicz <jakub@chodorowicz.com>

--- setup-scripts/defaults.sh
+++ setup-scripts/defaults.sh
@@ -12,9 +12,26 @@ defaults write com.apple.dock "mineffect" -string "scale"
 killall Dock
 
 ### Finder
+
+# Show all files
 # https://macos-defaults.com/finder/appleshowallfiles.html
 defaults write com.apple.finder "AppleShowAllFiles" -bool "true"
 killall Finder
+
+# Show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Disable the warning when changing a file extension
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+# Avoid creating .DS_Store files on network or USB volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+# Use list view in all Finder windows by default
+# https://macos-defaults.com/finder/fxpreferredviewstyle.html
+# Four-letter codes for the other view modes: `icnv`, `clmv`, `glyv`
+defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
 
 ### Trackpad
 
@@ -32,6 +49,23 @@ defaults write com.apple.AppleMultitouchTrackpad "Clicking" -bool true
 # this setting is stored in ~/Library/Preferences/.GlobalPreferences.plist
 # view with: plutil -p ~/Library/Preferences/.GlobalPreferences.plist
 defaults write -g com.apple.trackpad.scaling -float 2.8
+
+### Autocorrection
+
+# Disable automatic capitalization
+defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+
+# Disable smart dashes
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+
+# Disable automatic period substitution
+defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
+
+# Disable smart quotes
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+
+# Disable auto-correct
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
 
 # Activate new settings
 /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
