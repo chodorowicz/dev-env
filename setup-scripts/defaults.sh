bf94beabf401ef673866e152025d899cf253ebe5
Jakub Chodorowicz <jakub@chodorowicz.com>
2025-05-25 08:37:50 +0200

    Add trackpad macOS settings

--- setup-scripts/defaults.sh
+++ setup-scripts/defaults.sh
@@ -1,6 +1,7 @@
 ##!/usr/bin/env bash
 
-# Dock
+### Dock
+
 # autohide dock
 # https://macos-defaults.com/dock/autohide.html
 defaults write com.apple.dock "autohide" -bool "true"
@@ -10,11 +11,24 @@ defaults write com.apple.dock "mineffect" -string "scale"
 
 killall Dock
 
-## Finder
+### Finder
 # https://macos-defaults.com/finder/appleshowallfiles.html
 defaults write com.apple.finder "AppleShowAllFiles" -bool "true"
 killall Finder
 
-# Trackpad
+### Trackpad
+
+# Three finger drag
 # https://macos-defaults.com/trackpad/trackpadthreefingerdrag.html
-defaults write com.apple.AppleMultitouchTrackpad "TrackpadThreeFingerDrag" -bool "true"
+defaults write com.apple.AppleMultitouchTrackpad "TrackpadThreeFingerDrag" -bool true
+defaults write com.apple.AppleMultitouchTrackpad "Dragging" -bool true
+
+# Tap to click
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write com.apple.AppleMultitouchTrackpad "Clicking" -bool true
+
+# Increase tracking speed
+defaults write -g com.apple.trackpad.scaling -float 2.8
+
+# Activate new settings
+/System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
