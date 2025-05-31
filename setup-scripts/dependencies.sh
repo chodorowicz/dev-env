4f2303df9a9f09a0823b9b7c423a9cf90fef5d8c
Jakub Chodorowicz <jakub@chodorowicz.com>
2025-05-31 19:41:00 +0200

    Install raycas script

--- setup-scripts/dependencies.sh
+++ setup-scripts/dependencies.sh
@@ -10,3 +10,22 @@ brew bundle --file=$HOME/Brewfile
 
 ## install deno
 asdf plugin add deno https://github.com/asdf-community/asdf-deno.git
+
+## Install raycast scripts
+raycast_scripts_dir=$HOME/.config/raycast-scripts
+if [ ! -d $raycast_scripts_dir ]; then
+    mkdir -p $raycast_scripts_dir
+fi
+
+if [ ! -d $raycast_scripts_dir/copy-markdown-url-and-title ]; then
+    git clone git@github.com:chodorowicz/copy-markdown-url-and-title.git $raycast_scripts_dir/copy-markdown-url-and-title
+fi
+
+pushd $raycast_scripts_dir/copy-markdown-url-and-title
+git pull
+popd
+
+
+
+
+
