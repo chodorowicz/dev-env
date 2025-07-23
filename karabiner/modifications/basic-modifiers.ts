9a6db9648e7c9c76686b7f736873e7e356213a53
Jakub Chodorowicz <jakub@chodorowicz.com>
2025-07-23 10:08:31 +0200

    Add capslock escape

--- karabiner/modifications/basic-modifiers.ts
+++ karabiner/modifications/basic-modifiers.ts
@@ -3,6 +3,6 @@ import { rule, map } from "karabiner_ts";
 export function basicModifiers() {
 	return rule("Modifiers").manipulators([
 		map("caps_lock", "left_command").toHyper().toIfAlone("caps_lock"),
-		map("caps_lock").toMeh(),
+		map("caps_lock").toMeh().toIfAlone("escape"),
 	]);
 }
