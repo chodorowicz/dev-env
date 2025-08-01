8f128444a9d8836f90727f638356aab8297b9780
Jakub Chodorowicz <jakub@chodorowicz.com>
2025-08-01 19:48:00 +0200

    create holdTapLayer r layer

--- karabiner/modifications/next-previous-entity.ts
+++ karabiner/modifications/next-previous-entity.ts
@@ -4,11 +4,53 @@ import {
 	withModifier,
 	withCondition,
 	ifApp,
-	duoLayer,
-	BasicManipulator,
+	FromAndToKeyCode,
+	KeyAlias,
 } from "karabiner.ts";
 import { togglePanelsGeneric } from "./ui-controls.ts";
-import { holdTapLayer } from "karabiner.ts-greg-mods";
+import { holdTapLayer, modTap } from "karabiner.ts-greg-mods";
+
+const qwertyLeftHandKeys: (FromAndToKeyCode | KeyAlias)[] = [
+	"q",
+	"w",
+	"e",
+	"r",
+	"t",
+	"a",
+	"s",
+	"d",
+	"f",
+	"g",
+	"z",
+	"x",
+	"c",
+	"v",
+	"b",
+];
+
+const qwertyRightHandKeys: (FromAndToKeyCode | KeyAlias)[] = [
+	"y",
+	"u",
+	"i",
+	"o",
+	"p",
+	"[",
+	"]",
+	"\\",
+	"h",
+	"j",
+	"k",
+	"l",
+	";",
+	"'",
+	"n",
+	"m",
+	",",
+	".",
+	"/",
+	"␣",
+	"⏎",
+];
 
 function entitiesNavigationConfig(previous: string, next: string) {
 	return [
@@ -46,32 +88,35 @@ export function nextPreviousEntity() {
 		]),
 		// duoLayer("e", "r").manipulators(entitiesNavigationConfig("u", "i")),
 		// duoLayer("e", "r").manipulators(togglePanelsGeneric("m", ",")),
-		holdTapLayer("r").permissiveHoldManipulators(
-			...withCondition(
-				ifApp({
-					file_paths: ["Google Chrome"],
-				})
-			)([
-				map("i").to("right_arrow", ["left_option", "left_command"]),
-				map("u").to("left_arrow", ["left_option", "left_command"]),
-			]),
-			...withCondition(
-				ifApp({
-					file_paths: ["Obsidian", "Code", "Cursor", "iTerm"],
-				})
-			)([
-				map("i").to("]", ["left_command", "left_shift"]),
-				map("u").to("[", ["left_command", "left_shift"]),
-			]),
-			...withCondition(
-				ifApp({
-					file_paths: ["Arc.app"],
-				})
-			)([
-				map("i").to("down_arrow", ["left_command", "left_option"]),
-				map("u").to("up_arrow", ["left_command", "left_option"]),
-			])
-		),
+		holdTapLayer("r")
+			.permissiveHoldManipulators(
+				...withCondition(
+					ifApp({
+						file_paths: ["Google Chrome"],
+					})
+				)([
+					map("i").to("right_arrow", ["left_option", "left_command"]),
+					map("u").to("left_arrow", ["left_option", "left_command"]),
+				]),
+				...withCondition(
+					ifApp({
+						file_paths: ["Obsidian", "Code", "Cursor", "iTerm"],
+					})
+				)([
+					map("i").to("]", ["left_command", "left_shift"]),
+					map("u").to("[", ["left_command", "left_shift"]),
+				]),
+				...withCondition(
+					ifApp({
+						file_paths: ["Arc.app"],
+					})
+				)([
+					map("i").to("down_arrow", ["left_command", "left_option"]),
+					map("u").to("up_arrow", ["left_command", "left_option"]),
+				])
+			)
+			.echoKeys(...[...qwertyLeftHandKeys, ...qwertyRightHandKeys]),
+		// ...modTap().from("s").modifiers()
 		// duoLayer("e", "r").manipulators(togglePanelsGeneric("m", ",")),
 	];
 }
